3cbc743843ebb521688b327420c47202f23c3527
Sagnella Franco <Fransagn@gmail.com>
2023-08-11 14:59:08 -0300

    first commit

---
+++ src/main/java/com/twa/mediospago/MediosPagoApplication.java
@@ -0,0 +1,13 @@
+package com.twa.mediospago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediosPagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediosPagoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twa/mediospago/MediosPagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twa.mediospago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediosPagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
